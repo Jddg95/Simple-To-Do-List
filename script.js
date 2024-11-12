154ded64326e1a1abf618b0cbf7d21756b9642a9
Juan Garcia <140569072+Jddg95@users.noreply.github.com>
2024-11-12 15:45:07 -0600

    linked script to html

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ const taskList = document.getElementById("task-list");
 
 submitButton.addEventListener("click", function () {
   const task = textInput.value; // gets value from input field
+  console.log("task");
 });
