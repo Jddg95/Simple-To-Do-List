18abf874dca6bc2514ef57d2d6744da3db4f040d
Juan Garcia <140569072+Jddg95@users.noreply.github.com>
2024-11-12 15:54:21 -0600

    add new list item with user input text

--- script.js
+++ script.js
@@ -5,4 +5,6 @@ const taskList = document.getElementById("task-list");
 submitButton.addEventListener("click", function () {
   const task = textInput.value; // gets value from input field
   console.log("task");
+  const newTaskItem = document.createElement("li"); // Creates a new list item element
+  newTaskItem.innerText = task; //adds task text to list item.
 });
